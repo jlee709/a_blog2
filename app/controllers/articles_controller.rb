96b73f55658373de2d50a9358726228ef86dc0ad
jlee <leejwg.3@gmail.com>
2016-03-17 08:18:03 +0000

    added show controler/flash msg/ updated layouts

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,27 @@
 class ArticlesController < ApplicationController
+   
     def new
         @article = Article.new
     end
     
     def create
-        #render plain: params[:article].inspect
-        @articles = Article.new(article_params)
-        @articles.save 
-        redirect_to articles_show(@article )
+        @article = Article.new(article_params)
+            if @article.save 
+                flash[:notice] = "Article was created"
+            redirect_to article_path(@article)
+    else 
+       
+        render 'new'
+        end
+        
+        def show
+            @article = Article.find(params[:id])
+        end
+        
     end
     
     private
+    
     def article_params
         params.require(:article).permit(:title, :description)
     end
