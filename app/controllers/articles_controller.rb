472f705c79afdebe061362b4763603790d5bc5d1
jlee <leejwg.3@gmail.com>
2016-05-02 06:02:06 +0000

    changed associations and user/article id to recognize which user created article etc

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,30 +15,35 @@ before_action :set_article, only: [:edit,:update,:show,:destroy]
         
     end
                 
+    
     def create
-    @article = Article.new(article_params)
-        if @article.save 
-            flash[:success] = "Article was created"
-            redirect_to article_path(@article)
-    else 
-            render 'new'
+   
+        @article = Article.new(article_params)
+        @article.user = User.first
+            if  @article.save 
+                flash[:success] = "Article was successfully created"
+                redirect_to article_path(@article)
+                
+            else
+                render 'new'
             end
-        end
+    end
     
     def update
        
         if @article.update(article_params) 
                 flash[:success] = "your article was updated"
-            redirect_to article_path(@article)
+                redirect_to article_path(@article)
         else 
                render 'edit'
-        end
-    
+    end
+
     def show
      
     end    
             
     end
+    
     
     def destroy
          
@@ -51,17 +56,18 @@ before_action :set_article, only: [:edit,:update,:show,:destroy]
     end
     
 
+    #private information keys
     
     private
     
-    def set_article
-        @article = Article.find(params[:id])
-    end
+        def set_article
+            @article = Article.find(params[:id])
+        end
+     
     
-    
-    def article_params
-        params.require(:article).permit(:title, :description)
-    end
+     def article_params
+         params.require(:article).permit(:title, :description)
+        end
     
 
 
--- app/models/concerns/article.rb
+++ app/models/concerns/article.rb
@@ -1,6 +1,13 @@
 class Article < ActiveRecord::Base 
-    validates :title, presence: true, length: {minimum: 3, maximum:50}
-    validates :description, presence: true,  length: {minimum: 5, maximum:50}
-    
+    belongs_to :user
+    validates  :title, presence: true, length: {minimum: 3, maximum:50}
+    validates  :description, presence: true,  length: {minimum: 5, maximum:50}
+    validates  :user_id, presence: true
+ 
+ #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+   # validates :email, presence: true, length: { maximum: 105 },
+   # uniqueness: { case_sensitive: false },
+   # format: { with: VALID_EMAIL_REGEX }
 
 end
