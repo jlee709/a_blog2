efc75f88245632d490b89682ad96083945e6fa74
jlee <leejwg.3@gmail.com>
2016-05-13 09:57:33 +0000

    save point before db purge

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
  class UsersController < ApplicationController 
     
+    def index
+        @user = User.all
+    end
+    
     def new
         @user = User.new
     end
@@ -31,14 +35,18 @@
             render 'edit'  
         end
         
+     def show
+         
+         @user = User.find(params[:id])
      end
-     
-    
-    
+
+     end
+
     
     private
+    
     def user_params 
         params.require(:user).permit(:username, :email, :password)
-     end 
+    end 
      
  end 
