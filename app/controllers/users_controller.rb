cc89fddaa9c5eb80943e54cb4afbf987ead6ab32
jlee <leejwg.3@gmail.com>
2016-05-26 13:53:37 +0000

    safe save before fixing bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,45 +8,39 @@
         @user = User.new
     end
      
-     def create
-       
-       @user = User.new(user_params)
+    def create
+        @user = User.new(user_params)
         if @user.save
             flash[:success] = "Welcome to The RTF blog! #{@user.username}:)"
             redirect_to articles_path
-        
-    else
-       render 'new' 
+        else
+            render 'new' 
         end
-        
-     end
-     
-     def edit
+    
+    def edit
         @user = User.find(params[:id])
-     end
+    end
      
-     def update
-         @user = User.find(params[:id])
-        
+    def update
+        @user = User.find(params[:id])
         if @user.update(user_params)
-            flash[:success] = "Your Account Was Updated Successfuly"
+           flash[:success] = "Your Account Was Updated Successfuly"
                 redirect_to articles_path 
         else
             render 'edit'  
         end
         
-     def show
-         
-         @user = User.find(params[:id])
+    def show
+        @user = User.find(params[:id])
+        @username = User.username
      end
 
-     end
 
-    
     private
     
     def user_params 
         params.require(:user).permit(:username, :email, :password)
     end 
-     
- end 
+    end
+    end  
+ end
