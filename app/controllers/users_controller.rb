cb43e8fa9fb064cc0ca450dff690e8bf03f9d34e
jlee <leejwg.3@gmail.com>
2016-05-27 07:02:28 +0000

    Added gravatar and fixed bugs with rendering users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,46 +1,40 @@
- class UsersController < ApplicationController 
-    
-    def index
-        @user = User.all
-    end
-    
-    def new
-        @user = User.new
-    end
-     
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            flash[:success] = "Welcome to The RTF blog! #{@user.username}:)"
-            redirect_to articles_path
-        else
-            render 'new' 
-        end
-    
-    def edit
-        @user = User.find(params[:id])
-    end
-     
-    def update
-        @user = User.find(params[:id])
-        if @user.update(user_params)
-           flash[:success] = "Your Account Was Updated Successfuly"
-                redirect_to articles_path 
-        else
-            render 'edit'  
-        end
-        
-    def show
-        @user = User.find(params[:id])
-        @username = User.username
-     end
+class UsersController < ApplicationController
 
+  def new 
+    @user = User.new
+  end
 
-    private
-    
-    def user_params 
-        params.require(:user).permit(:username, :email, :password)
-    end 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+        flash[:success] = "Welcome to the alpha blog #{@user.username}"
+        redirect_to articles_path
+    else
+        render 'new'
     end
-    end  
- end
+  end
+
+  def edit 
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end 
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+end
